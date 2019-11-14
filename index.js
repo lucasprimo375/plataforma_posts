0f8840cba213bb7b17d0c3a956b62c04733d1106
GabrielPassos25 <gabrielurano30@gmail.com>
2019-11-14 17:31:53 -0300

    adicionando algumas rotas e algumas páginas do front pra testar

--- index.js
+++ index.js
@@ -7,10 +7,38 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get("/", function(req, res) {
-	res.sendFile(__dirname + "/index.html");
+	res.sendFile(__dirname + "/front/index.html");
 });
 
 app.post("/login", function(req, res){
+	// validar login
+	console.log(req.body);
+	// mandar página inicial
+	res.sendFile(__dirname + "/front/pagina_inicial.html");
+});
+
+app.get("/cadastro", function(req, res){
+	res.sendFile(__dirname + "/front/cadastro.html");
+});
+
+app.post("/fazer_cadastro", function(req, res){
+	console.log(req.body);
+	// validar dados
+});
+
+app.post("/buscar_curso", function(req, res){
+
+});
+
+app.post("/adicionar_curso", function(req, res){
+	res.sendFile(__dirname + "/front/adicionar_curso.html");
+});
+
+app.post("/editar_perfil", function(req, res){
+
+});
+
+app.post("/acessar_categoria", function(req, res){
 	console.log(req.body);
 });
 
